2edcc4bcb753368f4680e1181c6e52db386cc21c
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-02-14 16:18:26 +0000

    Setup GET request and test endpoint for /api/articles/:article_id

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { sendTopics } = require('./controllers/topics-controller');
+const { sendTopics, sendArticles } = require('./controllers/topics-controller');
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/api/topics', sendTopics);
+
+app.get('/api/articles/:article_id', sendArticles);
 
 app.all('/*', (req, res) => {
   res.status(404).send({ message: 'path not found'});
